3b502e8833d04fcdd0a2b4559a5bd98c5515b777
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-29 14:29:01 +0100

    Added hit points in view

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -7,3 +7,13 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Ellis'
   end
 end
+
+feature 'Hit Points' do 
+  scenario 'view hit points' do 
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Ellis'
+    click_button 'Submit'
+    expect(page).to have_content 'Ellis - Hit Points: 0'
+  end
+end
