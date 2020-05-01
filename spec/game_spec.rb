b532e901d10e6846c98ab14516716900ed98a22b
Ellis Trickett <ellis.trickett@gmail.com>
2020-05-01 15:25:54 +0100

    Refactored global variable

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,22 @@
 
 describe Game do
 
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
 
-  let(:player_1) { double(:player) }
-  let(:player_2) { double(:player) }
+  let(:player_1) { double(:player_1) }
+  let(:player_2) { double(:player_2) }
+
+  describe '#player_1' do 
+    it 'returns its name' do 
+      expect(player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do 
+    it 'returns its name' do 
+      expect(player_2).to eq player_2
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
@@ -12,4 +24,5 @@ describe Game do
       game.attack(player_2)
     end
   end
+
 end
